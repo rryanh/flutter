9fd77987044d0d0302b451417a148b76af32d881
rryanh <ryanhuga@gmail.com>
2020-09-30 14:21:58 -0700

    Finished chapter 8

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,24 @@
 // 1. Need to import helper lib from flutter
 // 2. define main func to run 
-// 3. create new tect widget 
+// 3. create new text widget 
 // 4. take widget and get it on screen..?
+
+
+import 'package:flutter/material.dart';
+void main() {
+  // text widget
+  var app = MaterialApp(
+    home: Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          print('hey there');
+        },
+        child: Icon(Icons.add),
+      ),
+      appBar: AppBar(
+        title: Text('This is an app'),
+      ),
+    ),
+  );
+  runApp(app);
+}
