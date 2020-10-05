ad2e0eb34c8bd70f1c30b714e0b1816f5cee1a1b
rryanh <ryanhuga@gmail.com>
2020-10-05 10:22:03 -0700

    Chapter 11
    completed chapter 11

---
+++ lib/src/models/image_model.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class ImageModel{
+  int id;
+  String url;
+  String title;
+
+  ImageModel(this.id, this.url, this.title);
+
+  ImageModel.fromJson(Map<String, dynamic> parsedJson){
+    id = parsedJson['id'];
+    url = parsedJson['url'];
+    title = parsedJson['title'];
+  }
+}
